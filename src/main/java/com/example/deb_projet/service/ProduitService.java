f3ce365605826e665b7b3ca6b1e1e801c52ccaef
SossouGahEzechiel <ezecsossougah@gmail.com>
2022-04-17 13:52:40 +0000

    produit-category front-end

--- src/main/java/com/example/deb_projet/service/ProduitService.java
+++ src/main/java/com/example/deb_projet/service/ProduitService.java
@@ -16,23 +16,15 @@ public class ProduitService {
     @Autowired
     ProduitRepository produitRepository;
 
-    // Méthode de récupération d'un produit
     public List<Produit> all() {
-        /*Produit produit = new Produit();
-        Category category = new Category();
-        category.setId(2);
-        produit.setCategory(category);
-        return produitRepository.findAll(Example.of(produit));*/
         return produitRepository.findByCategoryNotNull();
     }
 
-    // Méthode d'insertion dans la base de donnée (Simple insertion et mise à jour de donneés)
     public void insert(Produit produit) {
         System.out.println(produit.getId());
         produitRepository.save(produit);
     }
 
-    // Méthode de récupération d'une instance de la classe
     public Produit get(int id) {
         Optional<Produit> optional = produitRepository.findById(id);
         if(optional.isPresent()){
@@ -44,7 +36,6 @@ public class ProduitService {
         throw new RuntimeException("An error occurred");
     }
 
-    // Méthode de suppression dans la base de données
     public void destroy(int id) {
         Optional<Produit> produitOptional = produitRepository.findById(id);
         if(produitOptional.isPresent())
