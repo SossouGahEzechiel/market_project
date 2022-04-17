6677996794ec4c3be7bd8f7ab6d4286e70c4bb4f
SossouGahEzechiel <ezecsossougah@gmail.com>
2022-04-17 09:08:01 +0000

    initial commit

---
+++ src/main/java/com/example/deb_projet/DebProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.deb_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebProjetApplication.class, args);
+	}
+
+}
