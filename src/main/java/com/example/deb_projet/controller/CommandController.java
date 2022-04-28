407e8e0bed6eec0886475e173cf303bc8346f249
docasdiva <89033337+docasdiva@users.noreply.github.com>
2022-04-28 17:35:40 +0000

    command create form

--- src/main/java/com/example/deb_projet/controller/CommandController.java
+++ src/main/java/com/example/deb_projet/controller/CommandController.java
@@ -1,27 +1,40 @@
 package com.example.deb_projet.controller;
 
 import com.example.deb_projet.service.CommandService;
+import com.example.deb_projet.service.ProduitService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("command")
 public class CommandController {
 
-    // @Autowired
-    // CommandService commandService;
+    @Autowired
+    ProduitService produitService;
 
-    // @GetMapping("index")
-    // public String index(Model model){
-    //     model.addAttribute("commands", commandService.all());
-    //     return "command/index";
-    // }
+    @GetMapping("produits")
+    public String index(Model model){
+        model.addAttribute("produits", produitService.all());
+        model.addAttribute("title", "Liste des articles");
+        return "user/produit_interface/index";
+    }
 
-    // @GetMapping("user/{id}/command")
-    // public String userCommandList(Model model){
-    //     return "";
-    // }
+    
+    
+    
+    @GetMapping("{id}/create")
+    public String create(Model model, @PathVariable("id") int id){
+        model.addAttribute("produit",produitService.get(id));
+        return "user/self/command/create";
+    }
+
+    @GetMapping("user/{id}/command")
+    public String userCommandList(Model model){
+        return "";
+    }
 }
--- src/main/java/com/example/deb_projet/controller/SaleController.java
+++ src/main/java/com/example/deb_projet/controller/SaleController.java
@@ -1,7 +1,16 @@
 package com.example.deb_projet.controller;
 
+import com.example.deb_projet.repository.SaleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("sale")
 public class SaleController {
+
+    @Autowired
+    SaleRepository saleRepository;
 }
